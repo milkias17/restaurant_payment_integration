4e4ee08076700f51685558a9a128ef4d83842230
@yordi68 <yordanos.yirgu1@gmail.com>
2023-06-29 00:06:15 +0300

    add routes for customer

--- customer.py
+++ customer.py
@@ -1,11 +1,36 @@
-from flask import Blueprint
-
+from flask import Blueprint, request, redirect, url_for, render_template
+from models import Customer
 from db import db_access
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
 customer_bp = Blueprint("restaurant", __name__, url_prefix="/restaurant")
 
 
 @customer_bp.route("/register", methods=["GET", "POST"])
 @db_access
-def register():
-    pass
+def register(db):
+    if request.method == "POST":
+        name = request.form['name']
+        phone = request.form['phone']
+        password = request.form['password']
+        customer = Customer(name=name, phone=phone, password=password) 
+        db.add(customer)
+        db.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
+@customer_bp.route("/login", methods=["GET", "POST"])
+@db_access
+def login(db: Session):
+    if request.method == "POST":
+        phone = request.form['phone']
+        password = request.form['password']
+        stmt = select(Customer).filter_by(phone=phone, password=password)
+        res = db.scalars(stmt)
+        if res:
+            return redirect(url_for('home'))
+
+    return render_template('login.html')
+
